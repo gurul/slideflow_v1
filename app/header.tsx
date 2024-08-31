b787e201d19239e5ef6548de14f8615c58775515
Chaitanya Chandra Sekhar <cscs24@cs.washington.edu>
2024-08-30 20:00:47 -0700

    padded + popup

--- app/header.tsx
+++ app/header.tsx
@@ -1,19 +1,28 @@
+"use client";
+
 import Link from "next/link";
 
-const Header = () => {
+interface HeaderProps {
+  onSignUpClick: () => void;
+}
+
+const Header: React.FC<HeaderProps> = ({ onSignUpClick }) => {
   return (
     <header className="w-full p-6 bg-white shadow-md flex justify-between items-center">
-      <h1 className="text-2xl font-bold text-blue-700">SlideFlow</h1>
+      <h1 className="text-2xl font-bold text-[#196095]">SlideFlow</h1>
       <nav className="flex space-x-4">
         <Link href="#" legacyBehavior>
-          <a className="text-gray-700">About</a>
+          <a className="text-gray-700 py-2">About</a>
         </Link>
         <Link href="#" legacyBehavior>
-          <a className="text-gray-700">Sign In</a>
+          <a className="text-gray-700 py-2">Sign In</a>
         </Link>
-        <Link href="#" legacyBehavior>
-          <a className="px-4 py-2 bg-blue-700 text-white rounded-lg">Sign Up</a>
-        </Link>
+        <button
+          className="px-4 py-2 bg-[#196095] text-white rounded-3xl"
+          onClick={onSignUpClick}
+        >
+          Sign Up
+        </button>
       </nav>
     </header>
   );
